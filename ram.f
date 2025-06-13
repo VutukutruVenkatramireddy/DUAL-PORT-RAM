06375a3ac6ca347be0f71cecdc4fbd037d008f66
VutukutruVenkatramireddy <160195669+VutukutruVenkatramireddy@users.noreply.github.com>
2025-06-13 09:36:26 +0530

    Create ram.f

---
+++ ram.f
@@ -0,0 +1,8 @@
+-top testbench
++define+ALLOWREDEFINITION
++access+rwc
+//+incdir+$UVM_HOME/src
++incdir+/cad/cadence/INCISIV152/tools/methodology/UVM/CDNS-1.1d/sv/src
+//+incdir+/home/Desktop/dualportram/uvm-1.1d/src
+
+./ram_pkg.svh
